305e63363741568f7a44fae22b5424bdc72edfb2
zhenweicui <18604287698@163.com>
2021-01-05 20:11:21 +0800

    update 带参数的装饰器， 通过*args, **kwargs

--- py_decorators.py
+++ py_decorators.py
@@ -10,7 +10,7 @@ def run_time(func):
         end_time = time.time()
         costtime = end_time - start_time
         print("function cost time {}".format(costtime))
-    return wrapper()
+    return wrapper
 @run_time
 def func1():
     time.sleep(1)
@@ -20,9 +20,26 @@ def func2():
     time.sleep(2)
 
 @run_time
-def func():
+def func3():
     time.sleep(3)
 
 #   带参数的装饰器
+def log(func):
+    def wrapper(*args, **kwargs):
+        print("现在执行{}方法".format(func.__name__))
+        func(*args, **kwargs)
+        print("{}方法执行完成".format(func.__name__))
+    return wrapper
+
+@log
+def add_func(a,b):
+    print("{} + {} = {}".format(a, b, a+b))
+
+add_func(10, 20)
+func1()
+func2()
+func3()
+
+
 
 #   自定义属性的装饰器
